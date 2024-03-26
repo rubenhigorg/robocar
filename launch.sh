5bb5a5590fab5f4e2a46d5ccd17f39d85031d348
Ruben Higuera <rhiguerat00@gmail.com>
2024-03-26 17:36:59 +0100

    Added launcher for nodes

--- launch.sh
+++ launch.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+source /home/lab/robocar/.venv/bin/activate
 source /home/lab/robocar/install/setup.sh
 ros2 launch teleop_twist_joy teleop-launch.py &
 ros2 run robocar_package robocar_node
