294f43d2da930ad37704c3fe8f77bc9c5c6f02ad
rhiguera <trrhco@gmv.com>
2024-05-22 18:41:37 +0200

    Pruebas y lane_detection

---
+++ pruebas/computer_vision/hsv_calibration.py
@@ -0,0 +1,42 @@
+import cv2
+import numpy as np
+
+# Ruta de la imagen
+ruta_imagen = "image.jpg"
+
+# Cargar la imagen en color
+imagen = cv2.imread(ruta_imagen)
+
+# Convertir la imagen a HSV
+imagen_hsv = cv2.cvtColor(imagen, cv2.COLOR_BGR2HSV)
+
+# Crear una ventana para los controles deslizantes
+cv2.namedWindow('Controles')
+
+# Crear los controles deslizantes para los valores de HSV
+cv2.createTrackbar('Hue', 'Controles', 0, 179, lambda x: None)
+cv2.createTrackbar('Saturation', 'Controles', 0, 255, lambda x: None)
+cv2.createTrackbar('Value', 'Controles', 0, 255, lambda x: None)
+
+while True:
+    # Obtener los valores de los controles deslizantes
+    h = cv2.getTrackbarPos('Hue', 'Controles')
+    s = cv2.getTrackbarPos('Saturation', 'Controles')
+    v = cv2.getTrackbarPos('Value', 'Controles')
+
+    # Crear un array con los valores de HSV
+    lower_hsv = np.array([h, s, v])
+    upper_hsv = np.array([179, 255, 255])
+
+    # Aplicar la máscara HSV a la imagen
+    mask = cv2.inRange(imagen_hsv, lower_hsv, upper_hsv)
+    imagen_resultado = cv2.bitwise_and(imagen, imagen, mask=mask)
+
+    # Mostrar la imagen resultado
+    cv2.imshow("Imagen resultado", imagen_resultado)
+
+    # Salir del bucle si se presiona la tecla 'q'
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cv2.destroyAllWindows()
---
+++ pruebas/computer_vision/threshold.py
@@ -0,0 +1,53 @@
+import cv2
+import numpy as np
+
+# Ruta de la imagen
+ruta_imagen = "image.jpg"
+
+# Cargar la imagen en color
+imagen = cv2.imread(ruta_imagen)
+
+# Convertir la imagen a HSV
+imagen_hsv = cv2.cvtColor(imagen, cv2.COLOR_BGR2HSV)
+
+# Definir los valores de HSV para la detección de las líneas de los carriles
+lower_hsv = np.array([110, 38, 56])
+upper_hsv = np.array([179, 255, 255])
+
+# Aplicar la máscara HSV a la imagen
+mask = cv2.inRange(imagen_hsv, lower_hsv, upper_hsv)
+
+# Convertir la máscara a una imagen binaria
+imagen_binaria = cv2.threshold(mask, 1, 255, cv2.THRESH_BINARY)[1]
+
+# Mostrar la imagen binaria
+cv2.imshow("Imagen binariaa", imagen_binaria)
+cv2.waitKey(0)
+
+
+# imagen_resultado = cv2.bitwise_and(imagen, imagen, mask=mask)
+
+# Convertir la imagen resultado a escala de grises
+# imagen_gris = cv2.cvtColor(imagen_resultado, cv2.COLOR_BGR2GRAY)
+
+# # Crear una ventana para los controles deslizantes
+# cv2.namedWindow('Controles')
+
+# # Crear los controles deslizantes para los valores de HSV
+# cv2.createTrackbar('Ajuste', 'Controles', 0, 255, lambda x: None)
+
+# while True:
+#     # Obtener los valores de los controles deslizantes
+#     a = cv2.getTrackbarPos('Ajuste', 'Controles')
+
+#     # Aplicar el umbral a la imagen en escala de grises
+#     _, imagen_umbral = cv2.threshold(imagen_gris, a, 255, cv2.THRESH_BINARY)
+
+#     # Mostrar la imagen umbral
+#     cv2.imshow("Imagen umbral", imagen_umbral)
+
+#     # Salir del bucle si se presiona la tecla 'q'
+#     if cv2.waitKey(1) & 0xFF == ord('q'):
+#         break
+
+# cv2.destroyAllWindows()
