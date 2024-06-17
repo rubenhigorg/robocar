1e36775fe22dc7ad1a07bd550947c61fd1929695
Ruben Higuera <rhiguerat00@gmail.com>
2024-06-17 16:21:57 +0200

    Added Hough algorithm a(selfDrivingVehicle dir)

--- launch.sh
+++ launch.sh
@@ -4,4 +4,6 @@ source /home/lab/robocar/src/install/setup.sh
 ros2 launch teleop_twist_joy teleop-launch.py &
 ros2 run robocar_pkg car_control_node &
 ros2 run robocar_pkg energy_node & 
+ros2 run robocar_pkg accelerometer_node & 
+ros2 run robocar_pkg distance_node & 
 ros2 run robocar_pkg camera_node
