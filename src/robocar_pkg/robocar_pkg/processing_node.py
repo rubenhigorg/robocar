80a919639ac85353581f032b12f27c7654b9ebed
Ruben Higuera <rhiguerat00@gmail.com>
2024-06-30 21:01:41 +0200

    video added

--- src/robocar_pkg/robocar_pkg/processing_node.py
+++ src/robocar_pkg/robocar_pkg/processing_node.py
@@ -11,7 +11,7 @@ from lib.tracker import Tracker
 
 class ProcessingNode(Node):
     def __init__(self):
-        frame = 0
+        self.frame = 0
         super().__init__('processing_node')
         self.subscription = self.create_subscription(
             Image,
@@ -42,12 +42,11 @@ class ProcessingNode(Node):
     
 
     def image_callback(self, msg):
-        global frame
         # 1. Reads image:
         cv_image = self.bridge.imgmsg_to_cv2(msg, desired_encoding='bgr8')
-        filename = f'/home/lab/robocar/pruebas/computer_vision/log/frame_{frame}.jpg'
+        filename = f'/home/lab/robocar/pruebas/computer_vision/log/frame_{self.frame}.jpg'
         cv2.imwrite(filename, cv_image)
-        frame += 1
+        self.frame += 1
 
         self.get_logger().info('Received an image, doing Lane object...')
 
